83461447639765b97ac53ddd7432410c2be74bd2
Vlad Cojocariu <408484@student.fontys.nl>
2021-03-16 22:22:45 +0200

    Location done

--- Android/app/src/main/java/com/example/workaholic/activities/SplashActivity.kt
+++ Android/app/src/main/java/com/example/workaholic/activities/SplashActivity.kt
@@ -29,7 +29,7 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed(
             {
                 startActivity(Intent(this@SplashActivity,
-                    MainActivity2::class.java))
+                    LoginActivity::class.java))
                 finish()
             }, 2500
         )
