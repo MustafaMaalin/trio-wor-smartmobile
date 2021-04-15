b64083d240006614b705cb3f4aca32fa8b356803
Vlad <vvlad169@gmail.com>
2021-04-15 19:12:32 +0000

    Added all the other pages

--- iOS/Empathy2/Empathy2/Views/AddPost.swift
+++ iOS/Empathy2/Empathy2/Views/AddPost.swift
@@ -22,58 +22,85 @@ struct AddPost: View {
     @State var submit = false
     
     var body: some View {
-        VStack
-        {
+        
+    
+       
             
             
             
-            NavigationView {
-                
-                Form {
+                NavigationView {
                     
-                    Image(uiImage: img)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width:320,height:200)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .clipped()
-                    
-                    Button(action:
-                            {
-                                self.isShowingImage.toggle()
-                                print("nruv")
-                            },
-                           label: {
-                            Text("Add a photo")
-                           })
-                        .sheet(isPresented: $isShowingImage, content: {
-                            imgPicker(isPresented: self.$isShowingImage,selectedImg: self.$img)
-                        })
-                    
-                Picker(selection: $selection, label: Text("Topic")) {
-                    Text("Depression").tag(1)
-                    Text("Unemployment").tag(2)
-                }
-                Section(header: Text("Title")) {
-                    TextField("Your title", text: $title)
-                }
-                
-                
-                Section(header: Text("Description")) {
-                    
-                    TextField("Your description", text: $description)
-                        .frame( height: 150, alignment: .topLeading)
+                    ZStack {
                         
-                }
-                
-                Button(action: {submit.toggle() }) {
-                    Text("Submit")
-                }
-                
+                        VStack {
+                            Form {
+                                
+                                Section(header: Text("select image for your story")) {
+                                    Image(uiImage: img)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width:320,height:200)
+                                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                                        .clipped()
+                                }
+                                
+                                Button(action:
+                                        {
+                                            self.isShowingImage.toggle()
+                                            print("nruv")
+                                        },
+                                       label: {
+                                        WeatherButton(title: "Add photo",
+                                                      textColor: .white,
+                                                      backgroundColor: .gray)
+                                       })
+                                    .sheet(isPresented: $isShowingImage, content: {
+                                        imgPicker(isPresented: self.$isShowingImage,selectedImg: self.$img)
+                                    })
+                                
+                                Section(header: Text("Choose Topic")) {
+                                    Picker(selection: $selection, label: Text("Topic")) {
+                                    Text("Depression").tag(1)
+                                    Text("Unemployment").tag(2)
+                                    }
+                                }
+                            Section(header: Text("Title")) {
+                                TextField("Your title", text: $title)
+                            }
+                            
+                            
+                            Section(header: Text("Description")) {
+                                
+                                TextField("Your description", text: $description)
+                                    .frame( height: 150, alignment: .topLeading)
+                                    
+                            }
+                            
+                            
+                            
+                                
+                        }
+                            Button(action: {submit.toggle() }) {
+                                WeatherButton(title: "Submit",
+                                              textColor: .white,
+                                              backgroundColor: .blue)
+                            }
+                            
+                        }
+                    }
+                        .navigationBarTitle("Post your story")
+                    
+                    
+                    
                     
             }
-            .navigationBarTitle("Post your story")
-        }        }
+                
+                
+            
+            
+            
+            
+        
     }
 }
 
@@ -126,5 +153,66 @@ struct imgPicker: UIViewControllerRepresentable{
 struct AddPost_Previews: PreviewProvider {
     static var previews: some View {
         AddPost()
+            .preferredColorScheme(.dark)
+    }
+}
+
+
+
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)    }
+}
+struct AddPhotoButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)    }
+}
+struct BackgroundTopicsRight: View {
+    
+    
+    var body: some View {
+        
+        
+        
+        LinearGradient(gradient: Gradient(colors: [.black, .blue]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+struct BackgroundTopicsLeft: View {
+    
+    
+    var body: some View {
+        
+        
+        
+        LinearGradient(gradient: Gradient(colors: [.black, .blue]),
+                       startPoint: .topTrailing,
+                       endPoint: .bottomLeading)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
